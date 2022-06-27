0453a6ac3bc51656e3fbaa306f19a5dba7e6804d
wgcotera <wgcotera@espol.edu.ec>
2022-06-27 06:05:00 -0500

    chore: ask the user for a phone number and validate

--- mips_code/main.asm
+++ mips_code/main.asm
@@ -1,6 +1,6 @@
 .data
 
-mInicial:       .asciiz         "***************\n*** BIENVENIDO A LA CABINA TELEFONICA **\n****************\n"   
+mInicial:       .asciiz         "******************************\n*** BIENVENIDO A LA CABINA TELEFONICA **\n*******************************\n"   
 
 fmonInp:        .asciiz         "Ingrese Monedas: "
 fmonErr:        .asciiz         "*-- MONEDA INCORRECTA --*\n"
@@ -22,7 +22,9 @@ zerof:          .float          0.0
 mione:          .float          -1.0
 val_coin:       .float          0.05, 0.10, 0.25, 0.50, 1.00
 
+val_num:        .byte           '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'
 phone:          .space          15
+empty:          .asciiz         " "
 
                 .text
 
@@ -40,19 +42,28 @@ main:
     li		        $s2, 5		                # $s0 = 5
     # Address of valid coins array
     la		        $s3, val_coin		        #  
-    # Phone number length
-    li		        $s4, 15		                # $s4 = 15
-      
-    
+    # Phone number max index
+    li		        $s4, 9		                # $s4 = 15
+    # Address of valid numbers array
+    la		        $s5, val_num		        # 
+    # \n
+    li		        $s6, 10		                # $s6 = 10
+    # Initialize stack pointer
+    li		        $sp, 0		                # $sp = 0
+        
 
-# ********** WELCOME MESSAGE ********** #
     # Display Welcome Message
     li		        $v0, 4		                # $v0 = 4
     la		        $a0, mInicial		        # 
     syscall  
 
+    jal		        GET_MONEY				    # jump to GET_MONEY and save position to $ra
+    
+    jal		        GET_NUMBER				    # jump to GET_NUMBER and save position to $ra
+    
 
-# *********** GET COINS *********** #
+# ************************ GET COINS *********************** #
+
 GET_MONEY:
 
     # Display "Ingrese Monedas: "
@@ -66,7 +77,13 @@ GET_MONEY:
     c.eq.s          $f0, $f31                   # if $f0 == $f31 
     bc1t            GET_MONEY_END
     # Else Validate the coin
+
+    addi	        $sp, $sp, -4			    # $sp = $sp + -4
+    sw		        $ra, 0($sp)		            #     
     jal		        VAL_COIN				    # jump to VAL_COIN and save position to $ra
+    lw		        $ra, 0($sp)		            # 
+    addi	        $sp, $sp, 4			        # $sp = $sp + 4
+
     beq		        $v1, $s0, GET_MONEY_INV     # if $v1 == $s0 then I_COIN    
     add.s           $f2, $f2, $f0               # $f1 = $f1 + $f0
     j               GET_MONEY                   # jump to GET_MONEY
@@ -89,24 +106,22 @@ GET_MONEY_END:
     mov.s 	        $f12, $f2                   # $f12 = $f1
     li		        $v0, 2                      # $v0 = 2
     syscall
-    j		        GET_NUMBER				    # jump to END_PROGRAM
+    jr		        $ra					        # jump to $ra    
     
-    
-# ********** VALIDATE COINS *********** #
+# ********************* VALIDATE COINS ********************* #
 
 VAL_COIN:
     li		        $t1, 0		                # $t1 = 0
     add	            $t2, $zero, $s3	            # $t2 = $zero + $s3
     
-    
-VAL_COIN_WHILE:
+VAL_COIN_LOOP:
     beq		        $t1, $s2, VAL_COIN_I        # if $t1 == $s0 then END_VAL_COIN
     lwc1            $f3, 0($t2)                 # $f3 = Number of val_coin array
     c.eq.s          $f0, $f3                    # if $a0 == $f3
     bc1t            VAL_COIN_V  
     addi	        $t1, $t1, 1			        # $t1 = $t1 + 1
     addi	        $t2, $t2, 4			        # $t2 = $t2 + 4
-    j		        VAL_COIN_WHILE				# jump to WHILE_VAL_COIN                    
+    j		        VAL_COIN_LOOP				# jump to WHILE_VAL_COIN                    
     
 VAL_COIN_I:
     li		        $v1, 0		                # $v1 = 0 invalid coin
@@ -116,12 +131,96 @@ VAL_COIN_V:
     li		        $v1, 1		                # $v1 = 1 valid coin
     jr		        $ra					        # jump to $ra   
 
-# ********** GET PHONE NUMBER *********** #
+# ******************* GET PHONE NUMBER ********************* #
 
+GET_NUMBER:
+    # Ask for phone number
+    li		        $v0, 4		                # $v0 = 4
+    la		        $a0, fnumInp		        # 
+    syscall
+
+    # Getting user's input
+    li		        $v0, 8		                # $v0 = 8
+    la		        $a0, phone		            # 
+    li		        $a1, 15		                # $a1 = 15  size of the array
+    syscall
+    
+    addi	        $sp, $sp, -4			    # $sp = $sp + -4
+    sw		        $ra, 0($sp)		            #  
+    jal		        VAL_PHONE                   # jump to VAL_NUM and save position to $ra
+    lw		        $ra, 0($sp)		            # 
+    addi	        $sp, $sp, 4			        # $sp = $sp + 4
+
+    beq		        $v1, $s1, END_PROGRAM	    # if $v1 == $s1 then END_PROGRAM
+    # Display  "*-- NUMERO INCORRECTO --*"
+    li		        $v0, 4		                # $v0 = 4
+    la		        $a0, fnumErr		        # 
+    syscall
+    j		        GET_NUMBER				    # jump to GET_NUMBER
+    
  
 
-# ********** TERMINAR PROGRAMA ********** #
+# ******************** VALIDATE PHONE ********************* #
+
+VAL_PHONE:
+    li		        $t0, -1		                # $t0 = 0   letter counter        
+    add		        $t1, $a0, $zero		        # $t1 = $a0 + $zero
+    
+VAL_PHONE_LOOP:
+    addi	        $t0, $t0, 1			        # $t0 = $t0 + 1
+    
+    add		        $t2, $t0, $t1		        # $t1 = $t0 + $a0 pointer to letter
+    bgt		        $t0, $s4, VAL_PHONE_I       # if $t0 > $s4 then VAL_NUM_I   if index > 9
+
+    # CHECK IF THE CURRENT CHARACTER IS A NUMBER
+    lb              $a0, 0($t2)                 #
+
+    addi	        $sp, $sp, -4			    # $sp = $sp + -4
+    sw		        $ra, 0($sp)		            #     
+    jal		        VAL_NUMBER				    # jump to VAL_NUMBER and save position to $ra
+    lw		        $ra, 0($sp)		            # 
+    addi	        $sp, $sp, 4			        # $sp = $sp + 4
+
+    # IF IT'S NOT A NUMBER --> VAL_PHONE_I
+    beq		        $v1, $s0, VAL_PHONE_I	    # if $v0 == $s0 then VAL_PHONE_I
+
+    # ELSE IF IT'S THE 9TH CHARACTER --> CHECK IF THE NEXT ONE IS \n
+    bne		        $t0, $s4, VAL_PHONE_LOOP	# if $t0 != $s4 then VAL_PHONE_LOOP
+    lb              $t3, 1($t2)                 #
+    bne		        $t3, $s6, VAL_PHONE_I	    # if $a0 != $s6 then VAL_PHONE_I
+
+    li              $v1, 1                      # $v1 = 1 valid coin
+    jr              $ra                         # jump to $ra    
+
+VAL_PHONE_I:
+    li              $v1, 0                      # $v1 = 0 invalid coin
+    jr              $ra                         # jump to $ra
+
+# ******************** VALIDATE NUMBER ********************* #
+
+VAL_NUMBER:
+
+    li		        $t3, 48		                # $t3 = 48  ascii 0
+    li		        $t4, 57		                # $t4 = 57  ascii 9
+
+    blt		        $a0, $t3, VAL_NUMBER_I      # if $a0 < $t3 then VAL_NUM_I
+    bgt		        $a0, $t4, VAL_NUMBER_I      # if $a0 > $t4 then VAL_NUM_I
+    li              $v1, 1                      # $v1 = 1 valid coin
+    jr              $ra                         # jump to $ra
+
+VAL_NUMBER_I:
+    li              $v1, 0                      # $v1 = 0 invalid coin
+    jr              $ra                         # jump to $ra
+
+
+# ******************** TERMINAR PROGRAMA ******************** #
+
 END_PROGRAM: 
     li		        $v0, 17		                # $v0 = 17
     li		        $a0, 0		                # $a0 = 0
+    syscall
+
+    # Ask for phone number
+    li		        $v0, 4		                # $v0 = 4
+    la		        $a0, costPmin		        # 
     syscall
